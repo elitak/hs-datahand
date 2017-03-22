d6d8f0eea90ac4ed8ccbb35cc2d89c8c0d078fdb
Eric Litak <elitak@gmail.com>
2017-03-22 00:52:29 -0700

    support alt pin layout

--- build+program.nixos.sh
+++ build+program.nixos.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 # WARNING run as root, or fix usb write perms!
+#makeFlags="CFLAGS=-DALT_PINS_1=1"
 NIX_PATH="$NIX_PATH:nixpkgs=/home/keb/np"
 `nix-build`/bin/hs-datahand | tee ../dhteensy/keymaps-elitak.h
-pushd ../dhteensy; nix-shell --pure --run 'make && echo Please put keyboard into flash mode. Waiting for it to appear... && teensy-loader-cli -mmcu=at90usb1286 -w dhteensy.hex && echo Success!' ; popd
+pushd ../dhteensy; nix-shell --pure --run "make clean && make $makeFlags && echo Please put keyboard into flash mode. Waiting for it to appear... && teensy-loader-cli -mmcu=at90usb1286 -w dhteensy.hex && echo Success!" ; popd
