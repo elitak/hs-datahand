7021dbf4a353aa63fb0d80ba7e92bd4690cf2fa2
Eric Litak <elitak@gmail.com>
2018-06-19 04:43:42 -0700

    dhteensy as submodule

--- build+program.nixos.sh
+++ build+program.nixos.sh
@@ -6,7 +6,6 @@ if [[ $UID -ne 0 ]]; then echo "WARNING: run with "sudo -E" or the flashing step
 
 
 #makeFlags="CFLAGS=-DALT_PINS_1=1" # Alternate pinout for datahand #2. datahand #1 uses default wiring, but "alt" wiring should be the standard moving forward, assuming i continue using the same microcontroller.
-$(nix-build --no-out-link)/bin/hs-datahand | tee ../dhteensy/keymaps-elitak.h
-pushd ../dhteensy
+$(nix-build --no-out-link)/bin/hs-datahand | tee ./dhteensy/keymaps-elitak.h
+cd ./dhteensy
 nix-shell --pure --run "make clean && make $makeFlags && teensy-loader-cli -vw -mmcu=at90usb1286 dhteensy.hex"
-popd
