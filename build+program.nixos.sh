c09b7f3a6fe62d8f43e3a5882f0b7d1ab0b16f7f
Eric Litak <elitak@gmail.com>
2018-06-19 04:53:48 -0700

    had wrong dhteensy branch
    sidenote: i dont ever remember why i branched 'stm32f10x', but now
    they're merged, for better or worse. My only guess is that it was to do
    with trying to replace or clean up the ./stm32f10x dir.

--- build+program.nixos.sh
+++ build+program.nixos.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
- 
+
 set -eo pipefail
 
 if [[ $UID -ne 0 ]]; then echo "WARNING: run with "sudo -E" or the flashing step will fail (FIXME usb dev permissions)"; fi
-
 
 #makeFlags="CFLAGS=-DALT_PINS_1=1" # Alternate pinout for datahand #2. datahand #1 uses default wiring, but "alt" wiring should be the standard moving forward, assuming i continue using the same microcontroller.
 $(nix-build --no-out-link)/bin/hs-datahand | tee ./dhteensy/keymaps-elitak.h
