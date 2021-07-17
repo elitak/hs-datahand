8ba25f95edc3ff8262ee4651098f339f3dd1c565
Eric Litak <elitak@gmail.com>
2021-07-16 22:18:08 -0700

    finally fixed that regression

--- build+program.nixos.sh
+++ build+program.nixos.sh
@@ -4,7 +4,9 @@ set -eo pipefail
 
 if [[ $UID -ne 0 ]]; then echo "WARNING: run with "sudo -E" or the flashing step will fail (FIXME usb dev permissions)"; fi
 
-makeFlags="CFLAGS=-DALT_PINS_1=1" # Alternate pinout for datahand #2. datahand #1 uses default wiring, but "alt" wiring should be the standard moving forward, assuming i continue using the same microcontroller.
+if [[ "$1" != "--old" ]]; then
+  makeFlags="CFLAGS=-DALT_PINS_1=1" # Alternate pinout for datahand #2. datahand #1 uses default wiring, but "alt" wiring should be the standard moving forward, assuming i continue using the same microcontroller.
+fi
 $(nix-build --no-out-link)/bin/hs-datahand | tee ./dhteensy/keymaps-elitak.h
 cd ./dhteensy
 nix-shell --pure --run "make clean && make $makeFlags && teensy-loader-cli -vw -mmcu=at90usb1286 dhteensy.hex"
